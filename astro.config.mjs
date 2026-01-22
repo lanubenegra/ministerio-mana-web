ebc36d93e187ce81556ef2f0d60ac42756289251
lanubenegra <santiquilva@gmail.com>
2026-01-21 21:45:50 -0500

    Fix Vercel adapter and ignore build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://www.ejemplo-ministeriomana.org',
